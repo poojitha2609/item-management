cdf2236dcc4898ec4a523010aa0defab242f7920
poojitha2609 <poojithakl2609@gmail.com>
2025-01-10 08:18:51 +0530

    Save local changes before merging

---
+++ src/main/java/com/example/demo/dto/ItemDTO.java
@@ -0,0 +1,18 @@
+package com.example.demo.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ItemDTO {
+    private String name;
+    private Long itemNo;
+    private String description;
+    private float price;
+    private int stockQty;
+
+}
---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.demo.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
+        return new ResponseEntity<>("Entity Not Found",null, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseEntity<>("Item cannot be deleted",null, HttpStatus.BAD_REQUEST);
+    }
+}
